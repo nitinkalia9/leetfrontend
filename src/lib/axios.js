e3cf85f90f7429d1ce43535682411de020842e9b
nitinkalia9 <nkalia421@gmail.com>
2025-06-07 16:11:38 +0530

    axios updated

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8080/api/v1" : "/api/v1",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:8080/api/v1"
+    : import.meta.env.REACT_APP_API_URL + "/api/v1",
   withCredentials: true,
 });
