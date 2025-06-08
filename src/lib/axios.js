e0fba225775a0ebecb92ed2dfb60800a69732fc9
nitinkalia9 <nkalia421@gmail.com>
2025-06-08 11:13:54 +0530

    update axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,5 +4,5 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? import.meta.env.VITE_API_URL + "/api/v1"
     : import.meta.env.VITE_API_URL + "/api/v1",
-  withCredentials: true,
+  //withCredentials: true,
 });
